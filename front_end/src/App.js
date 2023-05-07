edc7c03b94ed26173801c6859a5f6330a6d540ab
mahendra kolhe <mkolhe23@gmail.com>
2023-05-07 22:26:00 +0530

    added front end

---
+++ front_end/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Auth from './Components/Auth/Auth'
+import './App.css'
+import Header from './Components/Auth/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      {/* <Auth/> */}
+    </div>
+  );
+}
+
+export default App;
