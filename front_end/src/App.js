ef066d4bb950f2a271f8d15f5ff1686df525d24c
mahendra kolhe <mkolhe23@gmail.com>
2023-05-26 23:32:26 +0530

    new project started

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import './App.css'
-import HomePage from './Components/HomePage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import Home from './Components/Home/Home';
+import Category from './Components/Category/Category';
+import SingleProduct from './Components/SingleProduct/SingleProduct';
+import Newsletter from "./Components/Footer/Newsletter/Newsletter";
+import AppContext from "./utils/context";
 
 function App() {
-  return (
-    <div className="App">
-      <HomePage />
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <AppContext>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/category/:id" element={<Category />} />
+                    <Route path="/product/:id" element={<SingleProduct />} />
+                </Routes>
+                <Newsletter />
+                <Footer />
+            </AppContext>
+        </BrowserRouter>
+    );
 }
-
 export default App;
---
+++ front_end/src/Components/Home/Home.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { TbSearch } from 'react-icons/tb';
+import { CgShoppingCart } from 'react-icons/cg';
+import { AiOutlineHeart } from 'react-icons/ai';
+
+import Search from '../Header/Search/Search';
+import Cart from '../Cart/Cart';
+import { Context } from "../../utils/context";
+
+import "./Home.scss";
+const Home = () => {
+    return <div>Home</div>;
+};
+
+export default Home;
