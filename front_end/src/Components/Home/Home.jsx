596667e6ba27ab4348be1597f297323ab98cddcd
mahendra kolhe <mkolhe23@gmail.com>
2023-05-27 17:39:36 +0530

    header added

--- front_end/src/Components/Home/Home.jsx
+++ front_end/src/Components/Home/Home.jsx
@@ -1,13 +1,3 @@
-import { useEffect, useState, useContext } from "react";
-import { useNavigate } from "react-router-dom";
-
-import { TbSearch } from 'react-icons/tb';
-import { CgShoppingCart } from 'react-icons/cg';
-import { AiOutlineHeart } from 'react-icons/ai';
-
-import Search from '../Header/Search/Search';
-import Cart from '../Cart/Cart';
-import { Context } from "../../utils/context";
 
 import "./Home.scss";
 const Home = () => {
