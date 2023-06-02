22e5090b6388b65ca66ed90105ade779f109cd61
Mahendra Kolhe <mkolhe23@gmail.com>
2023-06-02 17:16:51 +0000

    img added

--- src/Components/Home/Category/Category.jsx
+++ src/Components/Home/Category/Category.jsx
@@ -1,4 +1,7 @@
-import cat1 from '../../../assets/category/cat-1.jpg';
+import cat1 from '../../../images/home1.jpg';
+import cat2 from '../../../images/home2.jpg';
+import cat3 from '../../../images/home3.jpg';
+import cat4 from '../../../images/home4.jpg';
 import "./Category.scss";
 const Category = () => {
     return (
@@ -8,13 +11,13 @@ const Category = () => {
                     <img src={cat1} alt="" />
                 </div>
                 <div className="category">
-                    <img src={cat1} alt="" />
+                    <img src={cat2} alt="" />
                 </div>
                 <div className="category">
-                    <img src={cat1} alt="" />
+                    <img src={cat3} alt="" />
                 </div>
                 <div className="category">
-                    <img src={cat1} alt="" />
+                    <img src={cat4} alt="" />
                 </div>
             </div>
         </div>
